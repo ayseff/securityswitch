1c284040f712d53adba201209a9da4570320d0fa
Matt Sollars <Matt Sollars@localhost>
2010-12-05 21:02:06 -0500

    Added a stub for the SecuritySwitchModule class.

---
+++ Source/SecuritySwitch/SecuritySwitchModule.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Web;
+
+
+namespace SecuritySwitch {
+	/// <summary>
+	/// Evaluates each request for the need to switch to HTTP/HTTPS.
+	/// </summary>
+	public class SecuritySwitchModule : IHttpModule {
+		/// <summary>
+		/// Initializes a module and prepares it to handle requests.
+		/// </summary>
+		/// <param name="context">
+		/// An <see cref="T:System.Web.HttpApplication"/> that provides access to the methods, properties, and events common to 
+		/// all application objects within an ASP.NET application.
+		/// </param>
+		public void Init(HttpApplication context) {
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Disposes of the resources (other than memory) used by the module that implements <see cref="T:System.Web.IHttpModule"/>.
+		/// </summary>
+		public void Dispose() {
+			throw new NotImplementedException();
+		}
+	}
+}
