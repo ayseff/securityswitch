f00da0e45ea38feb382aea32c5a2c6355f6080b0
Matt Sollars <Matt Sollars@localhost>
2012-06-16 14:31:06 -0400

    Updated the ExampleWebSite to showcase using the new logging features with log4net as well as NLog.

--- ExampleWebSite/Global.asax.cs
+++ ExampleWebSite/Global.asax.cs
@@ -1,26 +1,70 @@
 ﻿using System;
 using System.Web;
 
-using NLog;
-
 using SecuritySwitch;
 using SecuritySwitch.Configuration;
+
+using log4net;
+//using NLog;
+//using LogManager = NLog.LogManager;
 
 
 namespace ExampleWebSite {
 	public class Global : HttpApplication {
-		private static readonly NLog.Logger _logger = LogManager.GetLogger("SecuritySwitch");
+		#region Logging with NLog
+		
+		//// Get an NLog logger for SecuritySwitch actions.
+		//private static readonly NLog.Logger _logger = LogManager.GetLogger("SecuritySwitch");
+
+		//protected void Application_Start(object sender, EventArgs e) {
+		//    // Setup a log action in order to capture logs from SecuritySwitch.
+		//    // Here, we just pass-through the log message and level to NLog.
+		//    SecuritySwitch.Logger.SetLogAction(
+		//        (message, logLevel) => {
+		//            // Translate the SecuritySwitch LogLevel to NLog's LogLevel.
+		//            var translatedLogLevel = LogLevel.FromString(logLevel.ToString());
+		//            _logger.Log(translatedLogLevel, message);
+		//        });
+		//}
+
+		#endregion
+
+		#region Logging with log4net
+
+		// Get a log4net logger for SecuritySwitch actions.
+		private static readonly ILog _logger = LogManager.GetLogger("SecuritySwitch");
+
+		private static void LogSecuritySwitchAction(string message, Logger.LogLevel logLevel) {
+			switch (logLevel) {
+				case Logger.LogLevel.Debug:
+					if (_logger.IsDebugEnabled) {
+						_logger.Debug(message);
+					}
+					break;
+
+				case Logger.LogLevel.Info:
+					if (_logger.IsInfoEnabled) {
+						_logger.Info(message);
+					}
+					break;
+
+				case Logger.LogLevel.Warn:
+					if (_logger.IsWarnEnabled) {
+						_logger.Warn(message);
+					}
+					break;
+			}
+		}
 
 		protected void Application_Start(object sender, EventArgs e) {
+			log4net.Config.XmlConfigurator.Configure();
+
 			// Setup a log action in order to capture logs from SecuritySwitch.
-			// Here, we just pass-through the log message and level to NLog.
-			SecuritySwitch.Logger.SetLogAction(
-				(message, logLevel) => {
-					// Translate the SecuritySwitch LogLevel to NLog's LogLevel.
-					var translatedLogLevel = LogLevel.FromString(logLevel.ToString());
-					_logger.Log(translatedLogLevel, message);
-				});
+			// Here, we just pass-through the log message and level to log4net.
+			SecuritySwitch.Logger.SetLogAction(LogSecuritySwitchAction);
 		}
+
+		#endregion
 
 		protected void Session_Start(object sender, EventArgs e) {}
 
