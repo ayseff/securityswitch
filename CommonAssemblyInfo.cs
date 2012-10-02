2e8f2b4a20c67b32743b80c63e1e0215b33aecd5
Matt Sollars <Matt Sollars@localhost>
2012-10-02 00:44:33 -0400

    Created CommonAssemblyInfo.cs and linked to it from each project. Updated each project's AssemblyInfo.cs accordingly.

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyDescription(".NET libraries for automatically switching between HTTP and HTTPS protocols.")]
+[assembly: AssemblyProduct("Security Switch")]
+[assembly: AssemblyCopyright("Copyright (c) 2004-2012 Matt Sollars")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("4.1.4557.0")]
+[assembly: AssemblyFileVersion("4.1.4557.0")]
--- Source/SecuritySwitch/Properties/AssemblyInfo.cs
+++ Source/SecuritySwitch/Properties/AssemblyInfo.cs
@@ -8,40 +8,9 @@
 // =================================================================================
 using System;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Security;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Security Switch")]
-[assembly: AssemblyDescription(".NET libraries for automatically switching between HTTP and HTTPS protocols.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SecuritySwitch")]
-[assembly: AssemblyCopyright("Copyright © 2004-2012 Matt Sollars")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: CLSCompliant(true)]
 [assembly: AllowPartiallyTrustedCallers]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bba22832-9de3-48f8-b21f-1fc4010e735f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("4.1.4657.0")]
-[assembly: AssemblyFileVersion("4.1.4657.0")]
